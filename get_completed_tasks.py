8b3be0f9bfc4f6e0735459588ecef8c9c59910cf
Harini Kannan <hkannan@mit.edu>
2016-10-10 12:42:21 -0400

    updated functions to print body of email and send

--- get_completed_tasks.py
+++ get_completed_tasks.py
@@ -22,11 +22,11 @@ def get_date():
     date = str(now.year) + "-" + month + "-" + day
     return date
 
-credentials_file = 'email.txt'
+credentials_file = '/Users/harini/Dropbox/Code/emailWunderlist/email.txt'
 credentials = open(credentials_file).readlines()
 
-access_token = credentials[4]
-client_id = credentials[2]
+access_token = credentials[4].strip()
+client_id = credentials[2].strip()
 
 task_endpoint = 'https://a.wunderlist.com/api/v1/tasks?' 
 list_endpoint = 'https://a.wunderlist.com/api/v1/lists?'
@@ -34,7 +34,11 @@ list_params = urllib.urlencode({'access_token':access_token, 'client_id':client_
 list_resp = urllib2.urlopen(list_endpoint+list_params).read()
 all_lists = json.loads(list_resp)
 
-output_file = open('example.txt', 'w')
+output_file = open('/Users/harini/Dropbox/Code/emailWunderlist/example.txt', 'w')
+
+print("<html><head></head><body>", file=output_file)
+print("Hi Harini,<br>", file=output_file)
+print("Here are all the tasks you did today. Great work! :)<br>", file=output_file)
 
 for task_list in all_lists:
     list_id = task_list['id']
@@ -51,8 +55,16 @@ for task_list in all_lists:
             todays_tasks.append(task['title'])
 
     if len(todays_tasks) > 0:
+        print("<p><b>", file=output_file)
         print(task_list['title'], file=output_file)
+        print("</b>", file=output_file)
+        print("<br>", file=output_file)
+        index = 1
         for task in todays_tasks:
+            print(str(index) + ".", file=output_file)
             print(task, file=output_file)
+            print("<br>", file=output_file)
+            index = index + 1
         print("\n", file=output_file)
 
+print("</body></html>", file=output_file)
--- send_email.py
+++ send_email.py
@@ -1,5 +1,4 @@
 # Adapted from http://stackoverflow.com/questions/64505/sending-mail-from-python-using-smtp
-
 import datetime
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
@@ -7,9 +6,9 @@ from email.MIMEText import MIMEText
 
 msg = MIMEMultipart()
 
-message_file = 'example.txt'
+message_file = '/Users/harini/Dropbox/Code/emailWunderlist/example.txt'
 fp = open(message_file, 'rb')
-msg = MIMEText(fp.read())
+msg = MIMEText(fp.read(), 'html')
 fp.close()
 
 msg['From'] = 'harini.automated@gmail.com'
@@ -17,17 +16,14 @@ msg['To'] = 'hariniharini@gmail.com'
 
 now = datetime.datetime.now()
 date = str(now.month) + "/" + str(now.day) + "/" + str(now.year)
-msg['Subject'] = 'What you did today (' + date + ') :)' 
+msg['Subject'] = 'What you did today (' + date + ')' 
 
 mailserver = smtplib.SMTP('smtp.gmail.com',587)
-# identify ourselves to smtp gmail client
 mailserver.ehlo()
-# secure our email with tls encryption
 mailserver.starttls()
-# re-identify ourselves as an encrypted connection
 mailserver.ehlo()
 
-credentials_file = 'email.txt'
+credentials_file = '/Users/harini/Dropbox/Code/emailWunderlist/email.txt'
 credentials = open(credentials_file).readlines()
 email_address = credentials[0].strip()
 password = credentials[1].strip()
